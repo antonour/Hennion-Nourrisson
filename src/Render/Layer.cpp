bfe47d662c7dd26423f87b741f3c76b8d9205997
antonour <antoine.nourrisson@ensea.fr>
2016-11-09 20:25:31 +0100

    layer

--- src/Render/Layer.cpp
+++ src/Render/Layer.cpp
@@ -38,22 +38,49 @@ namespace Render{
         if (e->getStateEventID()==state::StateEventID::FILECHAR_LOADED){
             surface->generateMap(list, surface->fowltab);
             if (!surface->loadChar("../res/chicken_large.png", sf::Vector2u(125, 97),sf::Vector2u(48,48), &surface->fowltab[0], 40, 30)){
-                throw std::runtime_error("IMPOSSIBLE DE CHARGER LE FICHIER");
+                throw std::runtime_error("IMPOSSIBLE DE CHARGER LE FICHIER CHICKEN");
+            }
+            if (!surface->loadArrow("../res/arrow.png", sf::Vector2u(125, 97),sf::Vector2u(48,48), &surface->arrowtab[0], 40, 30)){
+                throw std::runtime_error("IMPOSSIBLE DE CHARGER LE FICHIER ARROW");
             }
         }
+        
         if (e->getStateEventID()==state::StateEventID::FILEMAP_LOADED){
             surface->generateMap(list, surface->fieldtab);
             if (!surface->loadMap("../res/tileset.png", sf::Vector2u(125, 97),sf::Vector2u(125,97), &surface->fieldtab[0], 40, 30)){
                 throw std::runtime_error("IMPOSSIBLE DE CHARGER LE FICHIER");
             }   
         }
+        
+        if (e->getStateEventID()==state::StateEventID::FOWL_SELECTED){
+            int i=0,X=0,Y=0;
+            for (state::Element* pers: list){
+                if (pers->getTypeID()==state::TypeID::FOWL){
+                    state::Fowl* poule;
+                    poule=reinterpret_cast<state::Fowl*>(pers);
+                    if (poule->getFowlStatus()==state::FowlStatus::ALIVE_FACE && poule->isSelected()){
+                       X=poule->getX();
+                       Y=poule->getY();
+                       surface->arrowtab[i-40]=0;
+                       surface->moveArrow((i-40)%40,(i-40)/40,X,Y-97,surface->arrowtab[i-40]);
+                    }
+                }
+                i++;
+            }
+        
+        }
+        
         if (e->getStateEventID()==state::StateEventID::FOWL_DEAD){
             int i=0;
+            int X=0,Y=0;
             for(state::Element* el:list){
                 if (el->getTypeID()==state::TypeID::FOWL){
                     state::Fowl* poulet;
                     poulet=reinterpret_cast<state::Fowl*>(el);
-                    if (poulet->getFowlStatus()==state::FowlStatus::DEAD){
+                    if (poulet->getFowlStatus()==state::FowlStatus::DEAD && poulet->isSelected()){
+                        X=poulet->getX();
+                        Y=poulet->getY();
+                        surface->arrowtab[i-40]=0;
                         if (surface->fowltab[i]%9==1){
                             surface->fowltab[i]+=7;
                         }
@@ -77,6 +104,7 @@ namespace Render{
                         }
                 
                         surface->kill(i%40,i/40,surface->fowltab[i]);
+                        surface->moveArrow((i-40)%40,(i-40)/40,X,Y-97,surface->arrowtab[i-40]);
                     }
                 }
                 i++;
@@ -88,12 +116,13 @@ namespace Render{
             for (state::Element* pers: list){
                 if (pers->getTypeID()==state::TypeID::FOWL){
                     state::Fowl* poule;
-                    X=pers->getX();
-                    Y=pers->getY();
-                    X+=3;
-                    pers->setX(X);
                     poule=reinterpret_cast<state::Fowl*>(pers);
-                    if (poule->getFowlStatus()==state::FowlStatus::ALIVE_RIGHT){
+                    if (poule->getFowlStatus()==state::FowlStatus::ALIVE_RIGHT && poule->isSelected()){
+                       X=poule->getX();
+                       Y=poule->getY();
+                       X+=3;
+                       poule->setX(X);
+                       surface->arrowtab[i-40]=0;
                        if (surface->fowltab[i]%9==1){
                            surface->fowltab[i]+=4;
                        }
@@ -109,6 +138,7 @@ namespace Render{
                        else
                            surface->fowltab[i]=surface->fowltab[i]-surface->fowltab[i]%9+1;
                        surface->moveFowl(i%40,i/40,X,Y,surface->fowltab[i]);
+                       surface->moveArrow((i-40)%40,(i-40)/40,X,Y-97,surface->arrowtab[i-40]);
                     }
                     
                 }
@@ -121,12 +151,13 @@ namespace Render{
             for (state::Element* pers: list){
                 if (pers->getTypeID()==state::TypeID::FOWL){
                     state::Fowl* poule;
-                    X=pers->getX();
-                    Y=pers->getY();
-                    X-=3;
-                    pers->setX(X);
                     poule=reinterpret_cast<state::Fowl*>(pers);
-                    if (poule->getFowlStatus()==state::FowlStatus::ALIVE_LEFT){
+                    if (poule->getFowlStatus()==state::FowlStatus::ALIVE_LEFT && poule->isSelected()){
+                       X=poule->getX();
+                       Y=poule->getY();
+                       X-=3;
+                       poule->setX(X);
+                       surface->arrowtab[i-40]=0;
                        if (surface->fowltab[i]%9==1){
                            surface->fowltab[i]+=1;
                        }
@@ -142,6 +173,45 @@ namespace Render{
                        else
                            surface->fowltab[i]=surface->fowltab[i]-surface->fowltab[i]%9+1;
                        surface->moveFowl(i%40,i/40,X,Y,surface->fowltab[i]);
+                       surface->moveArrow((i-40)%40,(i-40)/40,X,Y-97,surface->arrowtab[i-40]);
+                    }
+                }
+                i++;
+            }
+        }
+        
+        if (e->getStateEventID()==state::StateEventID::FOWL_STOP){
+            int i=0;
+            int X=0;
+            int Y=0;
+            for (state::Element* pers: list){
+                if (pers->getTypeID()==state::TypeID::FOWL){
+                    state::Fowl* poule;
+                    poule=reinterpret_cast<state::Fowl*>(pers);
+                    if (poule->getFowlStatus()==state::FowlStatus::ALIVE_FACE && poule->isSelected()==false){
+                       X=poule->getX();
+                       Y=poule->getY();
+                       surface->arrowtab[i-40]=1;
+                       if (surface->fowltab[i]%9==2){
+                           surface->fowltab[i]-=1;
+                       }
+                       else if (surface->fowltab[i]%9==3){
+                           surface->fowltab[i]-=2;
+                       }
+                       else if (surface->fowltab[i]%9==4){
+                           surface->fowltab[i]-=3;
+                       }
+                       else if (surface->fowltab[i]%9==5){
+                           surface->fowltab[i]-=4;
+                       }
+                       else if (surface->fowltab[i]%9==6){
+                           surface->fowltab[i]-=5;
+                       }
+                       else if (surface->fowltab[i]%9==7){
+                           surface->fowltab[i]-=6;
+                       }
+                       surface->moveFowl(i%40,i/40,X,Y,surface->fowltab[i]);
+                       surface->moveArrow((i-40)%40,(i-40)/40,X,Y-97,surface->arrowtab[i-40]);
                     }
                 }
                 i++;
