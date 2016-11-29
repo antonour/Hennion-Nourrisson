c3c9b1eb7a7ef261bec426a3c2bd431cc1e588a3
antonour <antoine.nourrisson@ensea.fr>
2016-11-29 15:52:39 +0100

    animation

--- src/Render/Animation.cpp
+++ src/Render/Animation.cpp
@@ -9,8 +9,10 @@
 #include <memory>
 #include "Animation.h"
 #include <SFML/Graphics.hpp>
+#include <ctime>
 
 using namespace std;
+using namespace sf;
 
 namespace Render{
     
@@ -36,12 +38,18 @@ namespace Render{
     }
     
     void Animation::animate(int i, int j, int X, int Y){
+        Clock C;
+        Time T;
         state::Direction d=this->getDirection();
         if (this->getAnimID()==AnimID::HIT){
                 for(int k=1;k<250;k++){
                     if (k%5==0){
                         this->surf->rotateWeapon(i%40,j,X,Y,this->surf->weapontab[i],k/5,d);
-                        this->update(0);
+                        T=C.getElapsedTime();
+                        while(!this->update(T.asMilliseconds())){
+                            T=C.getElapsedTime();
+                        }
+                        T=C.restart();
                     }
                 }
             this->surf->dispWeapon(i%40,j,X,Y,this->surf->weapontab[i]);
@@ -52,13 +60,21 @@ namespace Render{
                     this->surf->moveArrow((i-40)%40,(j-40)/40,X-(125*k)/194,Y-97-k/2,this->surf->arrowtab[i-40]);
                     this->surf->dispWeapon(i%40,j/40,X-(125*k)/194,Y-k/2,this->surf->weapontab[i]);
                     this->surf->moveFowl(i%40,j/40,X-(125*k)/194,Y-k/2,this->surf->fowltab[i]);
-                    this->update(0);
+                    T=C.getElapsedTime();
+                    while(!this->update(T.asMilliseconds())){
+                        T=C.getElapsedTime();
+                    }
+                    T=C.restart();
                 }
                 else{
                     this->surf->moveArrow((i-40)%40,(j-40)/40,X-(125*k)/194,Y-194+k/2,this->surf->arrowtab[i-40]);
                     this->surf->dispWeapon(i%40,j/40,X-(125*k)/194,Y-97+k/2,this->surf->weapontab[i]);
                     this->surf->moveFowl(i%40,j/40,X-(125*k)/194,Y-97+k/2,this->surf->fowltab[i]);
-                    this->update(0);
+                    T=C.getElapsedTime();
+                    while(!this->update(T.asMilliseconds())){
+                        T=C.getElapsedTime();
+                    }
+                    T=C.restart();
                 }
             }
         }
@@ -68,13 +84,21 @@ namespace Render{
                     this->surf->moveArrow((i-40)%40,(j-40)/40,X+(125*k)/194,Y-97-k/2,this->surf->arrowtab[i-40]);
                     this->surf->dispWeapon(i%40,j/40,X+(125*k)/194,Y-k/2,this->surf->weapontab[i]);
                     this->surf->moveFowl(i%40,j/40,X+(125*k)/194,Y-k/2,this->surf->fowltab[i]);
-                    this->update(0);
+                    T=C.getElapsedTime();
+                    while(!this->update(T.asMilliseconds())){
+                        T=C.getElapsedTime();
+                    }
+                    T=C.restart();
                 }
                 else{
                     this->surf->moveArrow((i-40)%40,(j-40)/40,X+(125*k)/194,Y-194+k/2,this->surf->arrowtab[i-40]);
                     this->surf->dispWeapon(i%40,j/40,X+(125*k)/194,Y-97+k/2,this->surf->weapontab[i]);
                     this->surf->moveFowl(i%40,j/40,X+(125*k)/194,Y-97+k/2,this->surf->fowltab[i]);
-                    this->update(0);
+                    T=C.getElapsedTime();
+                    while(!this->update(T.asMilliseconds())){
+                        T=C.getElapsedTime();
+                    }
+                    T=C.restart();
                 }
             }
         }
@@ -82,11 +106,11 @@ namespace Render{
     
     void Animation::sync (int64_t time){}
     
-    void Animation::update (int64_t time){
-        if (time==0){
-            this->window->clear(sf::Color(102,102,225,255));
-            this->window->draw(*surf);
-            this->window->display();
+    bool Animation::update (int64_t time){
+        if (time<=2){
+            return false;
         }
+        return true;
+        
     }
 }
